ee247beb8754f2365737c9f12a4b930feb33f56d
RAVI <joshiravi987@gmail.com>
2025-11-05 12:15:47 +0530

    added java files

--- Hibernateonetomany_manytoone/src/main/java/com/test/Runfile.java
+++ Hibernateonetomany_manytoone/src/main/java/com/test/Runfile.java
@@ -10,30 +10,51 @@ public class Runfile {
 public static void main(String[] args)
 {
 	Department dept = new Department();
-	dept.setDeptName("Mth");
+	dept.setDeptName("HR");
+	
+	Department dept1 = new Department();
+	dept1.setDeptName("IT");
 	Datadao dao=new Datadao();
 
 	Employee emp=new Employee();
-	emp.setEmpName("john");
-	emp.setEmpsal(1000000);
+	emp.setEmpName("DEV");
+	emp.setEmpsal(100000);
 	emp.setDepartment(dept);
 	
 	Employee emp1=new Employee();
-	emp1.setEmpName("xyz");
-	emp1.setEmpsal(200000);
+	emp1.setEmpName("DAKSH");
+	emp1.setEmpsal(210000);
 	emp1.setDepartment(dept);
 	
 	Employee emp2=new Employee();
-	emp2.setEmpName("abc");
-	emp2.setEmpsal(285000);
+	emp2.setEmpName("DARSHAK");
+	emp2.setEmpsal(305000);
 	emp2.setDepartment(dept);
+	
+	Employee emp3=new Employee();
+	emp3.setEmpName("JIGNESH");
+	emp3.setEmpsal(150000);
+	emp3.setDepartment(dept1);
+	
+	Employee emp4=new Employee();
+	emp4.setEmpName("KIRAN");
+	emp4.setEmpsal(250000);
+	emp4.setDepartment(dept1);
+	
 	ArrayList<Employee> emplist=new ArrayList<Employee>();
 	emplist.add(emp);
 	emplist.add(emp1);
 	emplist.add(emp2);
 	
+	ArrayList<Employee> emplist1=new ArrayList<Employee>();
+	emplist1.add(emp3);
+	emplist1.add(emp4);
+	
+	
 	dept.setEmployees(emplist);
+	dept1.setEmployees(emplist1);
 	dao.SaveDepartment(dept);
+	dao.SaveDepartment(dept1);
 	System.out.println("Data Saved !");
 }
 }
